fac8e8d13933f058391adfb2267910f8312f31ca
vitaxa <vitaxa93gamebox@gmail.com>
2025-04-14 22:08:18 +0300

    fix javax packages [2]

--- src/main/java/dev/crashteam/hermes/repository/DemoAccessRepository.java
+++ src/main/java/dev/crashteam/hermes/repository/DemoAccessRepository.java
@@ -1,13 +1,12 @@
 package dev.crashteam.hermes.repository;
 
 import dev.crashteam.hermes.model.domain.DemoAccessRequestEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
 
 @Repository
 public interface DemoAccessRepository extends JpaRepository<DemoAccessRequestEntity, Long> {
